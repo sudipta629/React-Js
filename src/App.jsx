5fad7c6d5fdfd73dd00f9f00266d05191e948a10
Sudipta Biswas <sudiptabiswas.ss6295@gmail.com>
2026-03-12 20:26:11 +0530

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
-import AppName from "./components/AppName";
-import AppTodo from "./components/AppTodo";
-import TodoItme1 from "./components/TodoItem1";
-import TodoItme2 from "./components/TodoItem2";
-import "./App.css";
 
-function app() {
+import './App.css'
+function App() {
+
   return (
-    <center className="todo-container">
-      <AppName />
-      <AppTodo />
-      <div className="todo-item">
-        <TodoItme1 />
-        <TodoItme2 />
-      </div>
-    </center>
-  );
+    <>
+    <h1>
+      Sudipta Biswas
+    </h1>
+    </>
+  )
 }
 
-export default app;
+export default App
